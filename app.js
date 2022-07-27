8648c0d04d15ef044f9ed72d9f73022b72a6f1b0
Dilean Barroso <dileanbarroso@gmail.com>
2022-07-26 22:41:07 -0400

    end video 9

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("hello  Vue!")
+const app = Vue.createApp({
+  // We can register data, functions, and template data
+  // template:'<h2>I am the Template</h2>'
+
+  data() {
+    return {
+      title: "The Final Empire",
+      author: "Jerry B",
+      age: 45,
+    };
+  },
+
+  methods: {
+    ChangeTitle(title) {
+      this.title = title;
+    },
+  },
+});
+
+app.mount("#app");
